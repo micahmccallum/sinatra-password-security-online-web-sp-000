cd884d1432ecb1c51ee9304f4a393178221a7795
micahmccallum <micahmccallum@outlook.com>
2021-01-12 19:40:47 -0500

    commit tree c5951c4c8da5435a40c268fa04e848524c24275c

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,11 @@
 
 ActiveRecord::Schema.define(version: 20180314210852) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
